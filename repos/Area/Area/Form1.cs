90ef2a11fd9ac86961130e8e077e21dffadbb495
mmlawre <mlawre13@students.kennesaw.edu>
2021-01-31 19:31:41 -0500

    new changes
    push changes

--- repos/Area/Area/Form1.cs
+++ repos/Area/Area/Form1.cs
@@ -17,18 +17,57 @@ namespace Area
             InitializeComponent();
         }
 
-        private void exitBtn_Click(object sender, EventArgs e)
+        private void radioBtn1_CheckedChanged(object sender, EventArgs e)
         {
-            Console.WriteLine("Exit Button was Pressed");
-            Application.Exit();
+
+            if (radioBtn1.Checked == true)
+
+            {
+                Console.WriteLine("Circle is selected");
+                Area.ActiveForm.Text = "Radius";
+                areaTxtbox.Visible = false;
+                widthTxtbox.Visible = false;
+
+            }
+
         }
+
+            
 
         private void calcAreabtn_Click(object sender, EventArgs e)
         {
             Console.WriteLine("Calc Area Button was Pressed");
-            
+
+            if (radioBtn1.Checked == true)
+
+            {
+                string fString = radiusTxtbox.Text;  //all data entered in a Textbox is entered as a String
+
+                int f = Int32.Parse(fString);   //convert the fString to an interger and store it in variable f
+
+                // now you can use f in a calculation
+
+                double A = (3.14) *  f * f;
+                string r = A.ToString();
+                areaTxtbox.Text = r;
+            }
+
+            if (areaTxtbox.Visible == false)
+                areaTxtbox.Visible = true;
+            else
+                areaTxtbox.Visible = true;
+
+
         }
 
+
+
+
+
+
+
+
+            //CLEAR BUTTON
         private void clearBtn_Click(object sender, EventArgs e)
         {
             Console.WriteLine("Clear Button was Pressed");
@@ -37,9 +76,25 @@ namespace Area
            areaTxtbox.Clear();
         }
 
-        private void radioBtn1_CheckedChanged(object sender, EventArgs e)
-        {
 
+
+
+
+
+            //EXIT BUTTON
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Exit Button was pressed.");
+
+
+            if (MessageBox.Show("Are you sure you want to Exit?",
+                               "Area Program Exit",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
+
     }
 }
